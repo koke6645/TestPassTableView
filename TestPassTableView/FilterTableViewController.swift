1aff8e3ab7920353cf3eb0e75ad780b05c7b045c
koke6645 <koke6645@gmail.com>
2015-02-22 19:20:33 -0600

    Add cell in filter

--- TestPassTableView/FilterTableViewController.swift
+++ TestPassTableView/FilterTableViewController.swift
@@ -10,9 +10,34 @@ import UIKit
 
 class FilterTableViewController: UITableViewController {
 
+    var categories : [String]?
+    var tags : [String]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        categories = [
+        "Cat: 1",
+        "Cat: 2",
+        "Cat: 3",
+        "Cat: 4",
+        "cat: 5"]
+        
+        tags = [
+        "Tag: 1",
+        "Tag: 2",
+        "Tag: 3",
+        "Tag: 4",
+        "Tag: 5",
+        "Tag: 6",
+        "Tag: 7",
+        "Tag: 8",
+        "Tag: 9",
+        "Tag: 10",
+        "Tag: 11",
+        "Tag: 12",
+        "Tag: 13"]
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -30,25 +55,42 @@ class FilterTableViewController: UITableViewController {
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Potentially incomplete method implementation.
         // Return the number of sections.
-        return 0
+        return 2
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
-        return 0
+        if section == 0 {
+            return categories!.count
+        } else {
+            return tags!.count
+        }
     }
 
-    /*
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as UITableViewCell
 
-        // Configure the cell...
-
-        return cell
+        if indexPath.section == 0 {
+            let categoryCell = tableView.dequeueReusableCellWithIdentifier("categoryCell", forIndexPath: indexPath) as UITableViewCell
+            let category = categories![indexPath.row]
+            categoryCell.textLabel?.text = category
+            return categoryCell
+        } else {
+            let tagCell = tableView.dequeueReusableCellWithIdentifier("tagCell", forIndexPath: indexPath) as UITableViewCell
+            let tag = tags![indexPath.row]
+            tagCell.textLabel?.text = tag
+            return tagCell
+        }
     }
-    */
 
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return "Categories"
+        } else {
+            return "Tags"
+        }
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
